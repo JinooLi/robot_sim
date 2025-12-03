8a70c82107c06b1053460b822f16d1b23af0c423
JinooLi <jinu1423@naver.com>
2025-12-03 16:43:14 +0900

    main.py 추가

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+from control import MyController
+from simulation import RobotSim
 
 
 def main():
-    print("Hello from robot-motion-plannig!")
+    controller = MyController()
+    sim = RobotSim(
+        controller=controller,
+        gravity=-9.81,
+        time_frequency=1000.0,
+        control_frequency=100.0,
+        simulation_duration=10.0,
+        control_type_str="torque",
+    )
+    print(sim.get_robot_info())
+    sim.simulate()
+    data_name = "log_traj"
+    sim.save_simulation_data(name=data_name)
+    print("시뮬레이션 데이터 저장 완료.")
+    sim.visualize(file_name=data_name, fps=50)
+    print("시뮬레이션 재생 완료.")
 
 
 if __name__ == "__main__":
     main()
-
